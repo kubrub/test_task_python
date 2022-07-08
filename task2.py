04e6b5c482106ab01292e5c9fb27a755880339cb
kubrub <keshka3455@gmail.com>
2022-07-08 12:22:31 +0300

    added error handling for task 2

--- task2.py
+++ task2.py
@@ -1,24 +1,31 @@
 def task2(title_format, text_format, file_path):
-    arr_res = []
-    with open(file_path, 'r') as fp:
-        lines = fp.readlines()
-        for line in lines:
-            line_index = lines.index(line)
-            line = line.strip()
 
-            if line.find(title_format) != -1 and line[0] == title_format[0] and (
-                    text_format == 'A' or text_format == ''):
-                res = {'msg': line[0:9],
-                       'type': line[8],
-                       'offset': line_index,
-                       'text': line[10:len(line)]}
-                arr_res.append(res)
+    try:
+        with open(file_path, 'r') as fp:
+            arr_res = []
+            lines = fp.readlines()
+            for line in lines:
+                line_index = lines.index(line)
+                line = line.strip()
+                string = "AEWI "
+                if line.find(title_format) == -1:
+                    return "Title format not found"
+                elif string.find(text_format) == -1:
+                    return "Text format not found"
+                elif line.find(title_format) != -1 and line[0] == title_format[0] and (
+                        text_format == 'A' or text_format == ''):
+                    res = {'msg': line[0:9],
+                           'type': line[8],
+                           'offset': line_index,
+                           'text': line[10:len(line)]}
+                    arr_res.append(res)
 
-            elif line.find(title_format) != -1 and line[0] == title_format[0] and text_format == line[8]:
-                res = {'msg': line[0:9],
-                       'type': line[8],
-                       'offset': line_index,
-                       'text': line[10:len(line)]}
-                arr_res.append(res)
-
-    return arr_res
+                elif line.find(title_format) != -1 and line[0] == title_format[0] and text_format == line[8]:
+                    res = {'msg': line[0:9],
+                           'type': line[8],
+                           'offset': line_index,
+                           'text': line[10:len(line)]}
+                    arr_res.append(res)
+            return arr_res
+    except EnvironmentError:
+        return "File not found"
