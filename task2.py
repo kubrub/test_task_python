1c885276b38b6ac113308a9f20996a2139ab19cf
kubrub <keshka3455@gmail.com>
2022-07-11 20:56:37 +0300

    used regular expressions for task2

--- task2.py
+++ task2.py
@@ -1,31 +1,50 @@
-def task2(title_format, text_format, file_path):
+import re
 
+
+def task2(title_format, text_format, file_path):
     try:
         with open(file_path, 'r') as fp:
             arr_res = []
             lines = fp.readlines()
+            string = "AEWI "
+            if string.find(title_format) == -1:
+                print('title format not found')
+
             for line in lines:
                 line_index = lines.index(line)
                 line = line.strip()
-                string = "AEWI "
-                if line.find(title_format) == -1:
-                    return "Sorry, title format not found, please, try one more time"
-                elif string.find(text_format) == -1:
-                    return "Sorry, text format not found, please, try one more time"
-                elif line.find(title_format) != -1 and line[0] == title_format[0] and (
-                        text_format == 'A' or text_format == ''):
-                    res = {'msg': line[0:9],
-                           'type': line[8],
-                           'offset': line_index,
-                           'text': line[10:len(line)]}
-                    arr_res.append(res)
-
-                elif line.find(title_format) != -1 and line[0] == title_format[0] and text_format == line[8]:
-                    res = {'msg': line[0:9],
-                           'type': line[8],
-                           'offset': line_index,
-                           'text': line[10:len(line)]}
-                    arr_res.append(res)
+                if title_format == 'A' or title_format == '':
+                    res = re.match(text_format + r"\d{5}", line)
+                    if res is not None:
+                        res1 = {'msg': line[0:9],
+                                'type': line[8],
+                                'offset': line_index,
+                                'text': line[10:len(line)]}
+                        arr_res.append(res1)
+                elif title_format == 'I':
+                    res = re.search(text_format + r"\d{5}" + title_format, line)
+                    if res is not None:
+                        res1 = {'msg': line[0:9],
+                                'type': line[8],
+                                'offset': line_index,
+                                'text': line[10:len(line)]}
+                        arr_res.append(res1)
+                elif title_format == 'E':
+                    res = re.search(text_format + r"\d{5}" + title_format, line)
+                    if res is not None:
+                        res1 = {'msg': line[0:9],
+                                'type': line[8],
+                                'offset': line_index,
+                                'text': line[10:len(line)]}
+                        arr_res.append(res1)
+                elif title_format == 'W':
+                    res = re.search(text_format + r"\d{5}" + title_format, line)
+                    if res is not None:
+                        res1 = {'msg': line[0:9],
+                                'type': line[8],
+                                'offset': line_index,
+                                'text': line[10:len(line)]}
+                        arr_res.append(res1)
             return arr_res
     except EnvironmentError:
         return "Sorry, file not found, please, try one more time"
